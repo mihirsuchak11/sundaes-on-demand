30e10d361ec815ce1534f2d9f01e7c33a094beac
Mihir Suchak <mihir.suchak11@gmail.com>
2021-08-08 15:37:46 +0530

    Added Summary Form

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import { useState } from "react";
-import "./App.css";
+// import { useState } from "react";
+// import "./App.css";
 
-export function replaceCamelWithSpaces(color) {
-  return color.replace(/\B([A-Z])\B/g, " $1");
-}
+// export function replaceCamelWithSpaces(color) {
+//   return color.replace(/\B([A-Z])\B/g, " $1");
+// }
 
-function App() {
-  const [buttonColor, setButtonColor] = useState("MediumVioletRed");
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+// function App() {
+//   const [buttonColor, setButtonColor] = useState("MediumVioletRed");
+//   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-  const newButtonColor =
-    buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
+//   const newButtonColor =
+//     buttonColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
+//   return (
+//     <div>
+//       <button
+//         disabled={isButtonDisabled}
+//         style={{ backgroundColor: isButtonDisabled ? "gray" : buttonColor }}
+//         onClick={() => setButtonColor(newButtonColor)}
+//       >
+//         Change to {newButtonColor}
+//       </button>
+//       <label htmlFor="checkbox">Disabled button</label>
+//       <input
+//         id="checkbox"
+//         defaultChecked={isButtonDisabled}
+//         value={isButtonDisabled}
+//         type="checkbox"
+//         onChange={(e) => setIsButtonDisabled(e.target.checked)}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import SummaryForm from "./pages/summary/SummaryForm";
+
+const App = () => {
   return (
     <div>
-      <button
-        disabled={isButtonDisabled}
-        style={{ backgroundColor: isButtonDisabled ? "gray" : buttonColor }}
-        onClick={() => setButtonColor(newButtonColor)}
-      >
-        Change to {newButtonColor}
-      </button>
-      <label htmlFor="checkbox">Disabled button</label>
-      <input
-        id="checkbox"
-        defaultChecked={isButtonDisabled}
-        value={isButtonDisabled}
-        type="checkbox"
-        onChange={(e) => setIsButtonDisabled(e.target.checked)}
-      />
+      <SummaryForm />
     </div>
   );
-}
+};
 
 export default App;
